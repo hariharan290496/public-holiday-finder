599c4bf058b690732ccb5c5755eb7575005ae543
Hariharan <hariharan@Hariharans-MacBook-Air.local>
2024-11-18 11:51:48 +0530

    Footer component initial implementation

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="text-center py-6 bg-blue-600 text-white">
+      <h1 className="text-3xl font-bold">Public Holiday Finder</h1>
+    </header>
+  );
+}
+
+export default Header;
